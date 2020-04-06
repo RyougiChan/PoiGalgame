a0fd70f7401ad2029de36cc805909379480386dd
Johnsen <ryougi.chan.fire@gmail.com>
2020-04-06 13:49:18 +0800

    KS Language Editor developing: sync

--- KsEditor/js/main.js
+++ KsEditor/js/main.js
@@ -1,15 +1,26 @@
 (() => {
     'use strict';
 
+    const KS_LINE_TEMPLATE = `
+    <div class="ks-line">
+        <input type="text" class="ks-input" placeholder="Line Text"><span class="remove">×</span>
+    </div>
+    `;
+
     let y_area_scale = 1;
     $('#y-area-operation').on('wheel', (evt) => {
         y_area_scale += event.deltaY * -0.001;
-console.log(y_area_scale)
-        // Restrict scale
         y_area_scale = Math.min(Math.max(.125, y_area_scale), 4);
-
-        // Apply scale transform
         $('#y-area-scaleable').css('transform', `scale(${y_area_scale})`);
+        $('#y-area-zoom-v > input').val((Math.floor(y_area_scale * 100)));
+    });
+
+    $('#y-area-zoom-v > input').on('change', (evt) => {
+        let v = parseInt($(evt.target).val());
+        if(!isNaN(v)) {
+            y_area_scale = v / 100;
+            $('#y-area-scaleable').css('transform', `scale(${y_area_scale})`);
+        }
     });
 
     $('#y-area-draggable').droppable({
@@ -24,7 +35,7 @@ console.log(y_area_scale)
     });
 
     $('.ks-widget').draggable({ 
-        containment: "#y-area-draggable", 
+        // containment: "#y-area-draggable", 
         scroll: false,
         start: function(event, ui) {
             // ui.position.left = 0;
@@ -43,5 +54,38 @@ console.log(y_area_scale)
     
         }
     });
+    $('.add-ks-line').on('click', function(evt) {
+        let $ks_action = $(evt.target).parent().parent().parent();
+        $ks_action.append(KS_LINE_TEMPLATE);
+    });
+
+    let KsCode = (() => {
+        return {
+            add: {
+                action() {
+                
+                },
+                line() {
+    
+                },
+            },
+            update: {
+                action() {
+                
+                },
+                line() {
+    
+                },
+            },
+            remove: {
+                action() {
+                
+                },
+                line() {
+    
+                },
+            }
+        }
+    })();
 
 })();
--- KsEditor/js/y-widget.js
+++ KsEditor/js/y-widget.js
@@ -2,4 +2,26 @@
     'use strict';
 
     $(".y-widget" ).parent().draggable({ opacity: 0.7, helper: "clone" });
+
+    $('.color-picker').farbtastic(function (c) {
+        console.log(c);
+        let $target_con = $(this.wheel).parent().parent().parent().parent();
+        let $hex_color = $($($target_con.children().get(1)).children().get(0));
+        $hex_color.val(c);
+        $hex_color.css('color', c);
+    });
+
+    $('.ks-color-picker').on('click', function(evt) {
+        let color_picker = $(evt.target).children().get(0);
+        if(!color_picker) return;
+        if($(color_picker).css('display') === 'block') {
+            $(color_picker).fadeOut(100);
+        } else {
+            $(color_picker).fadeIn(100);
+        }
+    });
+
+    $('.ks-action').delegate('.remove', 'click', function(evt) {
+        $(this).parent().remove();
+    });
 })();
